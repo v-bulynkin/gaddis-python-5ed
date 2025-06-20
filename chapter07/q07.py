1e2fc2b0a89271ff073817660c327c854ac93b6b
Viacheslav Bulynkin <56349712+v-bulynkin@users.noreply.github.com>
2025-06-20 17:15:55 +0300

    Экзамен на получение водительских прав

--- chapter07/q07.py
+++ chapter07/q07.py
@@ -4,14 +4,20 @@
 CHECKPOINT = 15
 
 def main():
-  #reference = ['a','c','a','a','d','b','c','a','c','b','a','d','c','a','d','c','b','b','d','a']
+	#reference = ['a','c','a','a','d','b','c','a','c','b','a','d','c','a','d','c','b','b','d','a']
 	reference = get_answers(r'C:\temp\reference.txt')
 	student_solution = get_answers(r'C:\temp\student_solution.txt')
-	score = get_score(reference,student_solution)
+	score, wrong_answers = get_score(reference,student_solution)
+	
 	if score >= CHECKPOINT:
 		print(f'Поздравляю, вы правильно ответили на {score} вопросов и успешно сдали экзамен!')
 	else:
-		print(f'К сожалению, вы ответили правильно только на {score} вопросов. Необходима пересдача.')
+		print(f'К сожалению, вы ответили правильно только на {score} из минимальных {CHECKPOINT} вопросов. Необходима пересдача.')
+	
+	if wrong_answers:
+		print(f'Номера неверных ответов: {wrong_answers}')
+	else:
+		print('Неверных ответов нет, отлично!')
 
 def get_answers(file_path):
 	answers_file = open(file_path, 'r')
@@ -22,11 +28,18 @@ def get_answers(file_path):
 	
 def get_score(reference,student_solution):
 	score = 0
+	wrong_answers = []
 	for i in range(len(reference)):
 		if student_solution[i].lower() == reference[i]:
 			score += 1
-	return score
+		else:
+			wrong_answers.append(i+1)
+	return score, wrong_answers
 
 if __name__ == '__main__':
 	main()
 
+'''
+Поздравляю, вы правильно ответили на 15 вопросов и успешно сдали экзамен!
+Номера неверных ответов: [4, 7, 10, 11, 20]
+'''
